0a664a1268e070c0911d380081d7cf2cc9dbcad7
xoansdhkd <xoansdhkd@gmail.com>
2022-03-18 09:06:55 +0900

    TM

--- Q_10819/TM_10819.java
+++ Q_10819/TM_10819.java
@@ -1,13 +1,14 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class TM_10819 {
-  public static int max = 0;
-
   public static void main(String[] args) {
     int n;
     int[] a;
     int[] temp;
     boolean[] visited;
+    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
     Scanner sc = new Scanner(System.in);
     n = sc.nextInt();
     a = new int[n];
@@ -18,17 +19,18 @@ public class TM_10819 {
       a[i] = sc.nextInt();
     }
 
-    sequence(a, n, temp, 0, visited);
-    System.out.println(max);
+    sequence(a, n, temp, 0, visited, priorityQueue);
+    System.out.println(priorityQueue.poll());
   }
 
-  private static void sequence(int[] a, int n, int[] temp, int index, boolean[] visited) {
+  private static void sequence(int[] a, int n, int[] temp, int index, boolean[] visited,
+      PriorityQueue<Integer> priorityQueue) {
     if (index == n) {
       int sum = 0;
       for (int i = 0; i < n - 1; i++) {
         sum += Math.abs(temp[i] - temp[i + 1]);
       }
-      max = Math.max(max, sum);
+      priorityQueue.offer(sum);
       return;
     }
 
@@ -38,7 +40,7 @@ public class TM_10819 {
       }
       visited[i] = true;
       temp[index] = a[i];
-      sequence(a, n, temp, index + 1, visited);
+      sequence(a, n, temp, index + 1, visited, priorityQueue);
       visited[i] = false;
     }
   }
