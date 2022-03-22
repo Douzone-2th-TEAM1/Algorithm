5f241bb99a6d6947c4209578b33f9eff1ae83578
xoansdhkd <xoansdhkd@gmail.com>
2022-03-22 22:11:02 +0900

    Feat: TM_10816.java

---
+++ Q_10816/TM_10816.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class TM_10816 {
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    HashMap<String, Integer> map = new HashMap<>();
+
+    int N = Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    while (st.hasMoreTokens()) {
+      String token = st.nextToken();
+      map.put(token, map.getOrDefault(token, 0) + 1);
+    }
+
+    int K = Integer.parseInt(br.readLine());
+
+    st = new StringTokenizer(br.readLine());
+    while (st.hasMoreTokens()) {
+      String token = st.nextToken();
+      sb.append(map.getOrDefault(token, 0)).append(" ");
+    }
+
+    System.out.println(sb);
+  }
+}
