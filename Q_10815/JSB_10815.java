0da40d366f4e4f572ae012ab7838cdfd683449cc
seungbeom613 <97654242+seungbeom613@users.noreply.github.com>
2022-03-25 01:21:35 +0900

    JSB

---
+++ Q_10815/JSB_10815.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.util.*;
+ 
+public class SB_10815 {
+
+	public static void main(String[] args) throws Exception{
+		int N,M=0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		String[]strNum=br.readLine().split(" ");
+		M = Integer.parseInt(br.readLine());
+		String[]strmNum=br.readLine().split(" ");
+		StringBuilder sb = new StringBuilder();
+		Arrays.sort(strNum);
+
+		for(int j=0;j<M;j++){
+			sb.append((Arrays.binarySearch(strNum,strmNum[j])>=0)?1+" ":0+" ");
+		}
+		System.out.print(sb);
+	}
+
+}
