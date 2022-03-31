d8df831a50a66f4ba53ba530247c6eb1f81e2cd0
chyeon97 <40657327+chyeon97@users.noreply.github.com>
2022-03-31 19:24:01 +0900

    CY

---
+++ Q_4358/CY_4358.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+
+public class Q_4358 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    HashMap<String, Integer> dic = new HashMap<>();
+    StringBuffer sb = new StringBuffer();
+    int totalCount = 0;
+
+    String input = br.readLine();
+    while (true) {
+      dic.put(input, dic.getOrDefault(input, 0) + 1);
+      totalCount++;
+
+      input = br.readLine();
+      if (input == null || input.length() <= 0)
+        break;
+      
+    }
+    
+    Object[] keys = dic.keySet().toArray();
+    Arrays.sort(keys);
+
+    for (Object key : keys) {
+      int count = dic.get(key.toString());
+      double answer = (double) (count * 100.0) / totalCount;
+
+      sb.append(key + " " + String.format("%.4f", answer) + "\n");
+    }
+    System.out.println(sb);
+  }
+}
