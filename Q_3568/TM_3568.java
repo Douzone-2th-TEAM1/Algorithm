475f39fdc288fb92e599581884804ab6f296b00c
xoansdhkd <xoansdhkd@gmail.com>
2022-04-05 23:56:36 +0900

    TM

---
+++ Q_3568/TM_3568.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class TM_3568 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String varType;
+    StringBuilder exType = new StringBuilder();
+    StringBuilder varName = new StringBuilder();
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " |,|;");
+    varType = st.nextToken();
+
+    while (st.hasMoreTokens()) {
+      String token = st.nextToken();
+      for (char ch : token.toCharArray()) {
+        if (ch == '&' || ch == '[' || ch == ']' || ch == '*') {
+          exType.append(ch);
+        } else {
+          varName.append(ch);
+        }
+      }
+      exType.reverse();
+
+      System.out
+          .println(
+              varType + exType.toString().replace('[', '@').replace(']', '[').replace('@', ']') + " " + varName + ";");
+
+      exType.setLength(0);
+      varName.setLength(0);
+    }
+  }
+}
