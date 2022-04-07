b20e52b729603bd08a26171035adb4c85d7973b9
xoansdhkd <101532769+xoansdhkd@users.noreply.github.com>
2022-04-07 09:07:10 +0900

    Update TM_1966.java

--- Q_1966/TM_1966.java
+++ Q_1966/TM_1966.java
@@ -9,7 +9,7 @@ public class TM_1966 {
   public static void main(String[] args) throws NumberFormatException, IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuffer sb = new StringBuffer();
-    int N = Integer.valueOf(br.readLine());
+    int N = Integer.parseInt(br.readLine());
 
     for (int i = 0; i < N; i++) {
 
