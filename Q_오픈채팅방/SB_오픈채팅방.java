02b17bf730a12146e47029cf5f244d212a2b4671
Seungbeom <97654242+seungbeom613@users.noreply.github.com>
2022-04-11 00:15:00 +0900

    Create SB_오픈채팅방.java

---
+++ Q_오픈채팅방/SB_오픈채팅방.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] record) {
+        String[] answer;
+        ArrayList<String> list = new ArrayList<>();
+        HashMap<String, String> users = new HashMap<>();
+        
+        for(int i=0; i<record.length; i++) {
+            String[] user = record[i].split("\\s");
+            if(!user[0].equals("Leave"))
+                users.put(user[1], user[2]);
+        }
+        
+        for(int i=0; i<record.length; i++) {
+            String[] s = record[i].split(" ");
+            if(s[0].equals("Enter")) {
+                list.add(users.get(s[1]) + "님이 들어왔습니다.");
+            }
+            else if(s[0].equals("Leave")) {
+                list.add(users.get(s[1]) + "님이 나갔습니다.");
+            }
+        }
+        answer = list.toArray(new String[list.size()]);
+        
+        return answer;
+    }
+}
