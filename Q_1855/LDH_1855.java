e628db05a8f8e49817b7565df0bd46f37222c023
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-04-27 19:40:46 +0900

    Update LDH_1855.java

--- Q_1855/LDH_1855.java
+++ Q_1855/LDH_1855.java
@@ -1,7 +1,8 @@
 package BOJ_1855;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,7 +15,6 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         String[][] s = new String[row][col];
-     
 
         for (int i = 0; i < row; i++) {
             if (i % 2 == 0) {
@@ -36,4 +36,5 @@ public class Main {
 
         System.out.println(sb);
 
+    }
 }
